dd50c22ba00cef319fc1c1155f0fa2dc78883da0
Daniel Roithmeier <roithmeierda@gmail.com>
2024-04-14 01:17:49 +0200

    adjusted url for ingredients

--- src/app/foodstuffs/shared/services/foodstuff.service.ts
+++ src/app/foodstuffs/shared/services/foodstuff.service.ts
@@ -26,12 +26,12 @@ export class FoodstuffService {
 
   getAllFoodstuffs(): Observable<Foodstuff[]> {
     console.debug('GET: fetching all foodstuffs ...');
-    return this.http.get<Foodstuff[]>(backendUrl + '/ingredients');
+    return this.http.get<Foodstuff[]>(backendUrl + '/foodstuffs');
   }
 
   getFoodstuffById(id: number): Observable<Foodstuff> {
     console.debug('GET: fetching foodstuff by id "' + id + '" ...');
-    return this.http.get<Foodstuff>(backendUrl + '/ingredients/' + id);
+    return this.http.get<Foodstuff>(backendUrl + '/foodstuffs/' + id);
   }
 
   patchFoodstuff(
@@ -40,32 +40,32 @@ export class FoodstuffService {
   ): Observable<Foodstuff> {
     console.debug('PATCH: patching foodstuff "' + id + '" ...');
     return this.http.patch<Foodstuff>(
-      backendUrl + '/ingredients/' + id,
+      backendUrl + '/foodstuffs/' + id,
       updates
     );
   }
 
   postFoodstuff(foodstuff: Partial<Foodstuff>): Observable<Foodstuff> {
     console.debug('POST: posting foodstuff "' + foodstuff + '" ...');
-    return this.http.post<Foodstuff>(backendUrl + '/ingredients', foodstuff);
+    return this.http.post<Foodstuff>(backendUrl + '/foodstuffs', foodstuff);
   }
 
   deleteFoodstuff(id: number): Observable<number> {
     console.debug('DELETE: deleting foodstuff by id "' + id + '" ...');
-    return this.http.delete<number>(backendUrl + '/ingredients/' + id);
+    return this.http.delete<number>(backendUrl + '/foodstuffs/' + id);
   }
 
   fetchVerboseNames(): Observable<VerboseNames> {
     console.debug('GET: fetching foodstuff verbose names ...');
     return this.http.get<VerboseNames>(
-      backendUrl + '/ingredients-meta-data/verbose-names'
+      backendUrl + '/foodstuffs-meta-data/verbose-names'
     );
   }
 
   fetchUnitChoices(): Observable<UnitChoices> {
     console.debug('GET: fetching foodstuff unit choices ...');
     return this.http.get<UnitChoices>(
-      backendUrl + '/ingredients-meta-data/unit-choices'
+      backendUrl + '/foodstuffs-meta-data/unit-choices'
     );
   }
 }
