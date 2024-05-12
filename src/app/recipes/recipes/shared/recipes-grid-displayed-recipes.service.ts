a1fb494d46f031df7d775fe78c17e4869683ef9e
roithme0 <98558985+roithme0@users.noreply.github.com>
2024-05-12 19:26:27 +0200

    moved comments

--- src/app/recipes/recipes/shared/recipes-grid-displayed-recipes.service.ts
+++ src/app/recipes/recipes/shared/recipes-grid-displayed-recipes.service.ts
@@ -12,23 +12,23 @@ import { RecipeService } from '../../shared/services/recipe.service';
 import { Recipe } from '../../shared/interfaces/recipe';
 import { RecipesGridControlsService } from './recipes-grid-controls.service';
 
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
 })
+// fetch all recipes
+// filter recipes by search input
 export class RecipesGridDisplayedRecipesService {
-  // fetch all recipes
-  // filter recipes by search input
   private recipeService: RecipeService = inject(RecipeService);
   private recipesGridControlsService: RecipesGridControlsService = inject(
     RecipesGridControlsService
   );
 
   private recipes: WritableSignal<Recipe[]> = signal([]);
+  // apply search input to recipes
+  // sort recipes
   private _displayedRecipes: Signal<Recipe[]> = computed(() => {
-    // apply search input to recipes
-    // sort recipes
     var displayedRecipes = this.recipes();
     displayedRecipes = this.filterRecipesByNameOrOrigin(displayedRecipes);
     displayedRecipes = this.sortRecipes('name', displayedRecipes);
@@ -39,11 +39,10 @@ export class RecipesGridDisplayedRecipesService {
   private _loading: WritableSignal<boolean> = signal(true);
   private _error: WritableSignal<boolean> = signal(false);
 
-  private searchBy: Signal<string> =
-    this.recipesGridControlsService.searchBy;
+  private searchBy: Signal<string> = this.recipesGridControlsService.searchBy;
 
+  // track changes to recipes
   constructor() {
-    // track changes to recipes
     this.recipeService.recipes$.subscribe(() => {
       this.fetchRecipes();
     });
@@ -63,8 +62,8 @@ export class RecipesGridDisplayedRecipesService {
     return this._error;
   }
 
+  // fetch all recipes
   private async fetchRecipes() {
-    // fetch all recipes
     this._loading.set(true);
     this.recipeService.getAllRecipes().subscribe({
       next: (recipes) => {
@@ -74,18 +73,20 @@ export class RecipesGridDisplayedRecipesService {
       },
       error: (err) => {
         console.error('failed to fetch recipes: ', err);
-        this.snackBarService.open('Rezepte konnten nicht geladen werden', '',{duration: 5000});
+        this.snackBarService.open('Rezepte konnten nicht geladen werden', '', {
+          duration: 5000,
+        });
         this._error.set(true);
         this._loading.set(false);
       },
       complete: () => {
         this._loading.set(false);
-      }
+      },
     });
   }
 
+  // filter recipes by search input
   private filterRecipesByNameOrOrigin(recipes: Recipe[]): Recipe[] {
-    // filter recipes by search input
     const searchBy = this.searchBy();
     if (searchBy === '') {
       return recipes;
@@ -98,8 +99,8 @@ export class RecipesGridDisplayedRecipesService {
     });
   }
 
+  // sort recipes by field
   private sortRecipes(sortBy: string, recipes: Recipe[]): Recipe[] {
-    // sort recipes by field
     const sortedRecipes = [...recipes];
     return sortedRecipes.sort((a, b) => {
       if (sortBy === 'name') {
