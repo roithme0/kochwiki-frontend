6cea5411aa2b788ffdb14a8e9b6071c56cdad3f8
roithme0 <98558985+roithme0@users.noreply.github.com>
2024-05-12 19:50:40 +0200

    pull recipes from helper service instead of input

--- src/app/recipes/recipes/shared/recipes-grid-controls.service.ts
+++ src/app/recipes/recipes/shared/recipes-grid-controls.service.ts
@@ -1,13 +1,28 @@
-import { Injectable, Signal, WritableSignal, signal } from '@angular/core';
+import {
+  Injectable,
+  Signal,
+  WritableSignal,
+  inject,
+  signal,
+} from '@angular/core';
+
+import { RecipesGridHelperServiceService } from './recipes-grid-helper-service.service';
+import { Recipe } from '../../shared/interfaces/recipe';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipesGridControlsService {
+  private recipesGridHelperService = inject(RecipesGridHelperServiceService);
+
   private _searchBy: WritableSignal<string> = signal('');
 
   set searchBy(value: string) {
     this._searchBy.set(value);
+  }
+
+  get recipes(): Signal<Recipe[]> {
+    return this.recipesGridHelperService.recipes;
   }
 
   get searchBy(): Signal<string> {
