3d730af2699d6d286ec533fbbfde74e893a062ec
roithme0 <98558985+roithme0@users.noreply.github.com>
2024-05-12 12:51:09 +0200

    prepared header for mat dialogs

---
+++ src/app/shared/dialog-header/dialog-header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-dialog-header',
+  standalone: true,
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatIconModule],
+  templateUrl: './dialog-header.component.html',
+  styleUrl: './dialog-header.component.css',
+})
+export class DialogHeaderComponent {}
