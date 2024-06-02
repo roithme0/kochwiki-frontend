d90917bc02f6689b232318ad5ad1a5a56115988f
roithme0 <98558985+roithme0@users.noreply.github.com>
2024-06-03 00:45:57 +0200

    fixed chart only working after reload

--- src/app/recipes/recipe/recipe-macro-chart/recipe-macro-chart.component.ts
+++ src/app/recipes/recipe/recipe-macro-chart/recipe-macro-chart.component.ts
@@ -1,22 +1,14 @@
-import {
-  Component,
-  ElementRef,
-  Input,
-  Signal,
-  ViewChild,
-  computed,
-  inject,
-} from '@angular/core';
+import { Component, Input, Signal, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Recipe } from '../../shared/interfaces/recipe';
+import { LegendElement } from '../../../shared/interfaces/legend-element';
 
 import { MatCardModule } from '@angular/material/card';
 
 import Chart from 'chart.js/auto';
 
 import { LegendElementComponent } from './legend-element/legend-element.component';
-import { LegendElement } from '../../../shared/interfaces/legend-element';
 
 @Component({
   selector: 'app-recipe-macro-chart',
@@ -26,70 +18,66 @@ import { LegendElement } from '../../../shared/interfaces/legend-element';
   styleUrl: './recipe-macro-chart.component.css',
 })
 export class RecipeMacroChartComponent {
-  title = 'ng-chart';
-  chart: any = [];
-
   @Input() recipe!: Recipe;
 
-  legend: { [id: string]: LegendElement } = {
-    carbs: {
-      displayName: 'Kohlenhydrate',
-      color: 'rgb(19,154,155)',
-      valueAbsolute: null,
-      valuePercentage: null,
-    },
-    protein: {
-      displayName: 'Protein',
-      color: 'rgb(155, 255, 117)',
-      valueAbsolute: null,
-      valuePercentage: null,
-    },
-    fat: {
-      displayName: 'Fett',
-      color: 'rgb(255,97,97)',
-      valueAbsolute: null,
-      valuePercentage: null,
-    },
+  chart: Signal<any> = computed(
+    () =>
+      new Chart('canvas', {
+        type: 'doughnut',
+        options: {
+          cutout: '70%',
+        },
+        data: {
+          datasets: [
+            {
+              data: [this.recipe.carbs, this.recipe.protein, this.recipe.fat],
+              backgroundColor: [
+                this.legend['carbs']().color,
+                this.legend['protein']().color,
+                this.legend['fat']().color,
+              ],
+              borderWidth: 0,
+            },
+          ],
+        },
+      })
+  );
+
+  legend: { [id: string]: Signal<LegendElement> } = {
+    carbs: computed(() => {
+      return {
+        displayName: 'Kohlenhydrate',
+        color: 'rgb(19,154,155)',
+        valueAbsolute: this.recipe.carbs,
+        valuePercentage: this.calculateValuePercentage(
+          this.recipe,
+          this.recipe.carbs
+        ),
+      };
+    }),
+    protein: computed(() => {
+      return {
+        displayName: 'Protein',
+        color: 'rgb(155, 255, 117)',
+        valueAbsolute: this.recipe.protein,
+        valuePercentage: this.calculateValuePercentage(
+          this.recipe,
+          this.recipe.protein
+        ),
+      };
+    }),
+    fat: computed(() => {
+      return {
+        displayName: 'Fett',
+        color: 'rgb(255,97,97)',
+        valueAbsolute: this.recipe.fat,
+        valuePercentage: this.calculateValuePercentage(
+          this.recipe,
+          this.recipe.fat
+        ),
+      };
+    }),
   };
-
-  ngOnChanges() {
-    this.legend['carbs'].valueAbsolute = this.recipe.carbs;
-    this.legend['protein'].valueAbsolute = this.recipe.protein;
-    this.legend['fat'].valueAbsolute = this.recipe.fat;
-
-    this.legend['carbs'].valuePercentage = this.calculateValuePercentage(
-      this.recipe,
-      this.recipe.carbs
-    );
-    this.legend['protein'].valuePercentage = this.calculateValuePercentage(
-      this.recipe,
-      this.recipe.protein
-    );
-    this.legend['fat'].valuePercentage = this.calculateValuePercentage(
-      this.recipe,
-      this.recipe.fat
-    );
-
-    this.chart = new Chart('canvas', {
-      type: 'doughnut',
-      options: {
-        cutout: '70%',
-      },
-      data: {
-        datasets: [
-          {
-            data: [this.recipe.carbs, this.recipe.protein, this.recipe.fat],
-            backgroundColor: [
-              this.legend['carbs'].color,
-              this.legend['protein'].color,
-              this.legend['fat'].color,
-            ],
-            borderWidth: 0,
-          },
-        ],
-      },
-    });
-  }
 
   private calculateValuePercentage(
     recipe: Recipe,
