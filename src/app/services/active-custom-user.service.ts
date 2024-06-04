db16b54892ae9c47c8ca7b0b57bd29295e10e290
roithme0 <98558985+roithme0@users.noreply.github.com>
2024-06-04 23:44:05 +0200

    persist loggin via cookie

--- src/app/services/active-custom-user.service.ts
+++ src/app/services/active-custom-user.service.ts
@@ -10,16 +10,31 @@ import { CustomUser } from '../interfaces/custom-user';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { CookieService } from 'ngx-cookie-service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class ActiveCustomUserService {
   private snackBarService = inject(MatSnackBar);
+  private cookieService = inject(CookieService);
 
   private _activeCustomUser: WritableSignal<CustomUser | null> = signal(null);
 
+  constructor() {
+    const user = this.cookieService.get('activeCustomUser');
+    if (user) {
+      try {
+        this._activeCustomUser.set(JSON.parse(user));
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+
   set activeCustomUser(value: CustomUser) {
     this._activeCustomUser.set(value);
+    this.cookieService.set('activeCustomUser', JSON.stringify(value));
     this.snackBarService.open('Als "' + value.username + '" angemeldet', '', {
       duration: 5000,
     });
