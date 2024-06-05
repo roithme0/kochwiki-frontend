98e6ad14f9db1eeaa0b3380a42a46abd5e330600
roithme0 <98558985+roithme0@users.noreply.github.com>
2024-06-05 19:40:37 +0200

    enabled navigation to shopping list

--- src/app/shopping-list/shopping-list-page/shopping-list-page.component.ts
+++ src/app/shopping-list/shopping-list-page/shopping-list-page.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { PageHeaderService } from '../../services/page-header.service';
 
 @Component({
   selector: 'app-shopping-list-page',
   standalone: true,
   imports: [],
   templateUrl: './shopping-list-page.component.html',
-  styleUrl: './shopping-list-page.component.css'
+  styleUrl: './shopping-list-page.component.css',
 })
 export class ShoppingListPageComponent {
+  pageHeaderService = inject(PageHeaderService);
 
+  // set header values
+  ngOnInit() {
+    this.pageHeaderService.headline = 'Einkaufsliste';
+    this.pageHeaderService.back = '';
+    this.pageHeaderService.showBack = true;
+  }
 }
