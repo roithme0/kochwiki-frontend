c1548da1e418a95b7d6aa4e8885694b9e7d922dd
roithme0 <98558985+roithme0@users.noreply.github.com>
2024-06-06 00:41:40 +0200

    prepared shopping list functionality

--- src/app/recipes/recipe-page/ingredients-grid/ingredients-grid.component.ts
+++ src/app/recipes/recipe-page/ingredients-grid/ingredients-grid.component.ts
@@ -14,13 +14,21 @@ import { FoodstuffBackendService } from '../../../foodstuffs/services/foodstuff-
 
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-ingredients-grid',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatProgressBarModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './ingredients-grid.component.html',
   styleUrl: './ingredients-grid.component.css',
 })
