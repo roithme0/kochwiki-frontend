b32a887c9e6b17fae5ca98019412e90de8eab991
roithme0 <98558985+roithme0@users.noreply.github.com>
2024-06-05 11:17:43 +0200

    utilize foodstuff properties directly via ingredient getters

--- src/app/recipes/interfaces/ingredient.ts
+++ src/app/recipes/interfaces/ingredient.ts
@@ -1,10 +1,19 @@
 import { Foodstuff } from '../../foodstuffs/interfaces/foodstuff';
 
 export interface Ingredient {
+  [key: string]: any;
   id: number;
   index: number;
   foodstuff: Foodstuff;
   foodstuffId: number;
   amount: number;
   recipeId: number;
+  name: string;
+  brand: string | null;
+  unit: string;
+  unitVerbose: string;
+  kcal: number | null;
+  carbs: number | null;
+  protein: number | null;
+  fat: number | null;
 }
